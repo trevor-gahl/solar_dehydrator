c9dc7c22ab69576694966580b88f1ba617303275
Trevor Gahl <tgahl7@gmail.com>
2019-10-17 21:37:52 -0700

    v1.0 Final Commit

--- solar_dehydrator.ino
+++ solar_dehydrator.ino
@@ -4,12 +4,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
+// Define OLED parameterstemperature =dht.readTemperature();
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+// Define DHT temperature sensor parameters
 #define DHTPIN 2
 #define DHTTYPE DHT22
-
 
 // Declaration for SSD1306 display connected using software SPI (default case):
 #define OLED_MOSI   12
@@ -17,37 +19,26 @@
 #define OLED_DC    10
 #define OLED_CS    8
 #define OLED_RESET 9
+
+// Define temperature threshold
+#define THRESHOLD 57.2
+
+// Initialize the display and temperature sensor
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
-  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+  			OLED_MOSI, OLED_CLK, OLED_DC,
+			OLED_RESET, OLED_CS);
 DHT dht(DHTPIN,DHTTYPE);
 
-int chk;
+// Create global variables to hold sensor data
 float humidity;
 float temperature;
 bool fan_mode = false;
 
-
-void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
- dht.begin();
- // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
- pinMode(5,OUTPUT);
- digitalWrite(5, HIGH); // Active Low
- 
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  humidity = dht.readHumidity();
-  temperature =dht.readTemperature();
+// Function to print sensor data to OLED
+void screen_refresh(float humidity, float temperature, bool fan_mode)
+{
   display.clearDisplay();
-  display.setTextSize(1.8); // Draw 2X-scale text
+  display.setTextSize(1.8);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.print(F("Temperature(C): "));
@@ -64,19 +55,44 @@ void loop() {
   }else{
     display.println(F("OFF"));
   }
-  display.display();      // Show initial text
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" %, Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" Celsius");
-  if(temperature>57.2){
-    digitalWrite(5, LOW);   // turn the LED on (HIGH is the voltage level)
+  display.display();
+}
+
+// Setup code section
+void setup() {
+ 
+ // Initialize serial port to 9600 baud
+ Serial.begin(9600);
+ 
+ // Initialize temperature sensor
+ dht.begin();
+
+ // Check display status
+ if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+   Serial.println(F("SSD1306 allocation failed"));
+   for(;;); // Don't proceed, loop forever
+ }
+ 
+ // Initialize relay pin as output and initialize to off
+ pinMode(5,OUTPUT);
+ digitalWrite(5, HIGH); // Relay is active low
+}
+
+void loop() {
+  
+  // Read sensor data
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  
+  // Determine fan mode based on sensor data
+  if(temperature>THRESHOLD){
+    digitalWrite(5, LOW);  // Relay is active low
     fan_mode=true;
-    delay(2000);                       // wait for a second
   }else{
-    digitalWrite(5, HIGH);    // turn the LED off by making the voltage LOW
-    delay(2000);                       // wait for a second
+    digitalWrite(5, HIGH); // Relay is active low    
     fan_mode=false;
   }
+
+  // Print data to screen
+  screen_refresh(humidity, temperature, fan_mode);
 }
